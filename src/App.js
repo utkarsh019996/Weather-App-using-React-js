340c54263667c38a3951d7a846e4e1acf88ab926
Utkarsha Gouttam <152986439+utkarsh019996@users.noreply.github.com>
2024-04-20 10:00:53 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Weather from './Components/weather-app/Weather';
+function App() {
+  return (
+    <div className="App">
+      <Weather/>
+    </div>
+  );
+}
+
+export default App;
